5789deb45e025861c0113cb85f8a36ee6bf86a2b
Ronald Almeida <ronaldmateus785@gmail.com>
2024-04-12 17:11:43 -0300

    New styled-components configure

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,11 @@
-import Document, {
-    DocumentContext,
-    Head,
-    Html,
-    Main,
-    NextScript,
-} from "next/document";
+import type { DocumentContext, DocumentInitialProps } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
+    static async getInitialProps(
+        ctx: DocumentContext,
+    ): Promise<DocumentInitialProps> {
         const sheet = new ServerStyleSheet();
         const originalRenderPage = ctx.renderPage;
 
@@ -22,12 +19,7 @@ export default class MyDocument extends Document {
             const initialProps = await Document.getInitialProps(ctx);
             return {
                 ...initialProps,
-                styles: (
-                    <>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ),
+                styles: [initialProps.styles, sheet.getStyleElement()],
             };
         } finally {
             sheet.seal();
