97bb2d6fea447d496cd749ce5bf19d3ce1a81ee4
Ronald Almeida <ronaldmateus785@gmail.com>
2024-04-12 19:09:00 -0300

    Reset Html

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -2,6 +2,13 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     body {
-        background-color: red;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: none;
+    }
+
+    html {
+        font-size: 62.5%;
     }
 `;
