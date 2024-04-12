12f0dab242514e463a939ee06f5fbc8d58f52610
Ronald Almeida <ronaldmateus785@gmail.com>
2024-04-12 19:09:37 -0300

    Typeof Strapi Posts

---
+++ src/domain/posts/posts.ts
@@ -0,0 +1,92 @@
+export type PostID = number;
+
+export type PostAuthor = {
+    id: PostID;
+    name: string;
+    created_by: number;
+    updated_by: number;
+    created_at: string;
+    updated_at: string;
+};
+
+export type PostCategory = {
+    id: PostID;
+    name: string;
+    created_by: number;
+    updated_by: number;
+    created_at: string;
+    updated_at: string;
+};
+
+export type PostCreatedBy = {
+    id: PostID;
+    firstname: string;
+    lastname: string;
+    username: null;
+};
+
+export type PostCoverFormat = {
+    ext: string;
+    url: string;
+    hash: string;
+    mime: string;
+    name: string;
+    path: null;
+    size: number;
+    width: number;
+    height: number;
+    provider_metadata: {
+        public_id: string;
+        resource_type: string;
+    };
+};
+
+export type PostCover = {
+    id: PostID;
+    alternativeText: string;
+    caption: string;
+    previewUrl: null;
+    provider: string;
+    created_by: number;
+    updated_by: number;
+    created_at: string;
+    updated_at: string;
+    formats: {
+        thumbnail: PostCoverFormat;
+        small: PostCoverFormat;
+        medium: PostCoverFormat;
+        large: PostCoverFormat;
+    };
+};
+
+export type PostAttributes = {
+    title: string;
+    content: string;
+    slug: string;
+    author: PostAuthor;
+    category: PostCategory;
+    created_by: PostCreatedBy;
+    updated_by: PostCreatedBy;
+    created_at: string;
+    updated_at: string;
+    cover: PostCover;
+};
+
+export type PostData = {
+    id: PostID;
+    attributes: PostAttributes;
+};
+
+export type PostMetadata = {
+    pagination: {
+        page: number;
+        pageSize: number;
+        pageCount: number;
+        total: number;
+    };
+};
+
+export type AllPosts = {
+    data: Array<PostData>;
+    meta: PostMetadata;
+};
