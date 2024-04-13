f8fa827b6baaae8aefa94e6bd5938e1b17d93999
Ronald Almeida <ronaldmateus785@gmail.com>
2024-04-13 08:43:12 -0300

    Env Variables

---
+++ src/config/app-config.ts
@@ -0,0 +1,9 @@
+export const API_URL =
+    "https://hidden-plains-04614-9d8e1734463e.herokuapp.com/api";
+
+export const POST_URL =
+    "https://hidden-plains-04614-9d8e1734463e.herokuapp.com/api/posts?populate=*";
+
+export const SITE_NAME = "Blog Ronald";
+
+export const SITE_URL = "";
