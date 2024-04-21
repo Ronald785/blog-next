7fe49ad052c1ca5dd9158bbbdd21c547cd967857
Ronald Almeida <ronaldmateus785@gmail.com>
2024-04-21 01:01:18 -0300

    Grid Posts

--- src/container/HomePage/index.tsx
+++ src/container/HomePage/index.tsx
@@ -1,19 +1,23 @@
 import { Header } from "@/components/Header";
 import { MainContainer } from "@/components/MainContainer";
 import { AllPosts } from "@/domain/posts/posts";
+import { Container } from "./styles";
 
 export type HomePageProps = {
     posts: AllPosts;
 };
 
 export default function HomePage({ posts }: HomePageProps) {
+    console.log("posts: ", posts);
     return (
         <>
             <Header />
             <MainContainer>
-                {posts.data.map((post) => (
-                    <p key={post.id}>{post.attributes.title}</p>
-                ))}
+                <Container>
+                    {posts.data.map((post) => (
+                        <p key={post.id}>{post.attributes.title}</p>
+                    ))}
+                </Container>
             </MainContainer>
         </>
     );
--- src/container/HomePage/styles.tsx
+++ src/container/HomePage/styles.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    background-color: red;
+export const Container = styled.section`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(28rem, 1fr));
+    gap: ${({ theme }) => theme.spacings.large};
 `;
